3dfc0b24d6bf82f580dd0131ac1ac4fb5bfbbd11
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-30 19:36:20 +0300

    Add Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿void input(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+int positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    return count;
+}
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input(array);
+Console.Write($"Количество чисел больше 0: {positive(array)}");
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату b1: ");
+double b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату k1: ");
+double k1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату b2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату k2: ");
+double k2 = int.Parse(Console.ReadLine()!);
+
+double x = (b2-b1) / (k1-k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения прямых: [{x},{y}]");
